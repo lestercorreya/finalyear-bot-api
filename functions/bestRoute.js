c04dee2a6886cbc829a4b526d9ccda5d40f5bd6f
lester-correya <lester.correya@vahvah.com>
2022-06-22 17:42:38 +0530

    now atleast

--- functions/bestRoute.js
+++ functions/bestRoute.js
@@ -77,7 +77,12 @@ const bestRoute = (source,destination,obstacles) => {
             }
         }
 
-        returnable.push([mappings[curr_ori+required_ori][0],main[i],mappings[curr_ori+required_ori][1]])
+        if (mappings[curr_ori+required_ori][0] === "r" || mappings[curr_ori+required_ori][0] === "l" || mappings[curr_ori+required_ori][0] === "d") {
+            returnable.push([mappings[curr_ori+required_ori][0],main[i-1],mappings[curr_ori+required_ori][1]])
+            returnable.push(["s",main[i],mappings[curr_ori+required_ori][1]])
+        } else {
+            returnable.push("s",main[i-1],mappings[curr_ori+required_ori][1])
+        }
         curr_ori = required_ori
         curr_pos = main[i]
     }
